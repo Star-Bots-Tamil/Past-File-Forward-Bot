052743322d53c58d05d91cf780130424c7e7f070
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-23 11:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     OWNER_ID = os.environ.get("OWNER_ID", "1391556668")
     LIMIT = int(os.environ.get("LIMIT", "1000000000000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
-    SESSION = os.environ.get("SESSION", "BQC2tFkAh5dhyS7ng1gcrSCxJzz22culDpXboh8k8uGmVXnFYDJqDir_N0JMCOPvTL-7N2fxTAhkci8jxoQKtw0YZD70RTt8JWCb6entBonI1GvM8oshhiIat_Y1CeYpiSUzwbgbyg-Jd3j8s10bXwo-tO7-3Z5lS_W281abHsHAMQEXAro_T51WYn2C0BJPWdk3r2_dX0o4fqRSCYJyu6BDqwG6a_qz51QVjLWUW-euQ4oo4OQnOa6KXs-FWo9rIsutT_kYj5zpzMTqLwMKdDHQWBgnpwJYvshBhJBuWZ9quo69ENiMEQ-tWQkL4J3ad_hQjF_UenF_Ui8DXY2GRQEhactoSwAAAAE4UbnNAA")
+    SESSION = os.environ.get("SESSION", "")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001849017994"))
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
