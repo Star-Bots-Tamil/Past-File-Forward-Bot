11a07fd52b647e8e800d3fcc599f077590ad5a82
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-23 18:38:21 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,9 +3,10 @@ from config import Config
 
 class Translation(object):
   START_TXT = """<b>Hi {}!!</b>
-<i>I'm Simple Auto file Forward Bot
+<b>I'm Simple Auto file Forward Bot
 This Bot forward all files to One Public channel to Your Personal channel
-More details /help</i>"""
+More details /help</b>"""
+  CAPTION = "<b>{}</b>"
   HELP_TXT = """<b>Follow These Steps!!</b>
 <b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
@@ -21,7 +22,7 @@ More details /help</i>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>TRTECHGUIDE</code>
+<b>Credit :</b> <b>@Star_Bots_Tamil</b>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
