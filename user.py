1781a9d1490ae1db83bc8bb5e1559fb7b37c855d
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-23 18:25:16 +0530

    Update user.py

--- user.py
+++ user.py
@@ -10,7 +10,7 @@ from pyrogram import Client, __version__
 class User(Client):
     def __init__(self):
         super().__init__(
-            Config.BOT_SESSION,
+            Config.USER_SESSION,
             session_string=Config.SESSION,
             api_hash=Config.API_HASH,
             api_id=Config.API_ID,
