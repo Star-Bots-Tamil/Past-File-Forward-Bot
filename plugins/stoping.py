9b870c3698dda7a2134e80a88cb166b43acf21cc
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-24 06:43:45 +0530

    Update stoping.py

--- plugins/stoping.py
+++ plugins/stoping.py
@@ -13,11 +13,11 @@ async def stop_button(c: Client, cb: CallbackQuery):
     await cb.message.delete()
     await cb.answer()
     msg = await c.send_message(
-        text="<i>Trying To Stoping.....</i>",
+        text="<i>Trying To Stopping.....</i>",
         chat_id=cb.message.chat.id
     )
     await asyncio.sleep(5)
-    await msg.edit("<i>File Forword Stoped Successfully 👍</i>")
+    await msg.edit("<b><i>File Forword Stopped Successfully 👍🏻</i>\nTotal Forwarded Files :- <code>{files_count}</code> Files</b>")
     os.execl(sys.executable, sys.executable, *sys.argv)
     
 @Client.on_callback_query(filters.regex(r'^close_btn$'))
